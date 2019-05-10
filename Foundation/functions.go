1951b91e8824df73870eb2b0d1e2972ed7e1a75e
bonsoirzw <2627894288@qq.com>
2019-05-10 15:34:40 +0800

    first commit

---
+++ Foundation/functions.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println(add1(42,13))
+}
+func add(x int,y int) int  {
+	return x + y
+}
+func add1(x ,y int) int  {
+	return x + y
+}
---
+++ Foundation/type-interface.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main(){
+	v := 4 + 5i // i= 41 i = 3.14
+	fmt.Printf("v is the type of %T\n",v)
+}
